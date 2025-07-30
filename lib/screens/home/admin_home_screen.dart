8a4003e4a7f0733b6187f7252120ca7a7c09b1d6
aaryesha17 <aaryeshasingharya@gmail.com>
2025-07-30 17:40:45 +0530

    fix(ui): maximize admin dialog to 99% width/height and minimize padding for full mobile utilization

--- lib/screens/home/admin_home_screen.dart
+++ lib/screens/home/admin_home_screen.dart
@@ -876,20 +876,20 @@ class _AdminLeadsPageState extends State<_AdminLeadsPage> {
       context: context,
       builder: (BuildContext context) {
         return Dialog(
-          insetPadding: EdgeInsets.symmetric(horizontal: 4),
+          insetPadding: EdgeInsets.symmetric(horizontal: 2),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(16),
           ),
           child: SizedBox(
-            width: MediaQuery.of(context).size.width * 0.98,
+            width: MediaQuery.of(context).size.width * 0.99,
             height: MediaQuery.of(context).size.width < 600
-                ? MediaQuery.of(context).size.height * 0.98 + 10
+                ? MediaQuery.of(context).size.height * 0.99 + 10
                 : MediaQuery.of(context).size.height * 0.9,
             child: Column(
               children: [
                 // Header
                 Container(
-                  padding: EdgeInsets.all(12),
+                  padding: EdgeInsets.all(8),
                   decoration: BoxDecoration(
                     color: Colors.blue[50],
                     borderRadius: BorderRadius.only(
@@ -933,7 +933,7 @@ class _AdminLeadsPageState extends State<_AdminLeadsPage> {
                 // Content
                 Expanded(
                   child: SingleChildScrollView(
-                    padding: EdgeInsets.all(12),
+                    padding: EdgeInsets.all(8),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -5558,17 +5558,20 @@ class _LeadTableState extends State<LeadTable> {
       context: context,
       builder: (BuildContext context) {
         return Dialog(
+          insetPadding: EdgeInsets.symmetric(horizontal: 2),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(16),
           ),
           child: SizedBox(
-            width: MediaQuery.of(context).size.width * 0.9,
-            height: MediaQuery.of(context).size.height * 0.9,
+            width: MediaQuery.of(context).size.width * 0.99,
+            height: MediaQuery.of(context).size.width < 600
+                ? MediaQuery.of(context).size.height * 0.99 + 10
+                : MediaQuery.of(context).size.height * 0.9,
             child: Column(
               children: [
                 // Header
                 Container(
-                  padding: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(8),
                   decoration: BoxDecoration(
                     color: Colors.blue[50],
                     borderRadius: BorderRadius.only(
@@ -5612,7 +5615,7 @@ class _LeadTableState extends State<LeadTable> {
                 // Content
                 Expanded(
                   child: SingleChildScrollView(
-                    padding: EdgeInsets.all(20),
+                    padding: EdgeInsets.all(8),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
