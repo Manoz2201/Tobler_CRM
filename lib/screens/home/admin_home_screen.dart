c9566dfe2ab3623c12f81eec9e686b19146633d5
aaryesha17 <aaryeshasingharya@gmail.com>
2025-07-31 11:33:00 +0530

    feat(ui): increase admin mobile lead card width by 6px on each side to match sales layout

--- lib/screens/home/admin_home_screen.dart
+++ lib/screens/home/admin_home_screen.dart
@@ -4384,7 +4384,7 @@ class _LeadTableState extends State<LeadTable> {
 
   Widget _buildMobileTable() {
     return ListView.builder(
-      padding: EdgeInsets.symmetric(horizontal: 12),
+      padding: EdgeInsets.symmetric(horizontal: 10),
       itemCount: _filteredLeads.length,
       itemBuilder: (context, index) {
         final lead = _filteredLeads[index];
